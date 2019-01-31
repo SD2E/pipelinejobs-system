51cc9db76cbe226100a6ba34203f0054958dde92
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-01-31 17:18:06 -0600

    Updates

--- scripts/stage-docs.sh
+++ scripts/stage-docs.sh
@@ -4,6 +4,8 @@ for X in demo-jobs-reactor-app pipelines-manager jobs-manager jobs-indexer jobs-
 do
     mkdir -p docs/$X
     cp -f $X/README.rst docs/$X/
+    cp -f $X/*.json docs/$X/
+    cp -f $X/*.jsonschema docs/$X/
     if [ -d $X/schemas ]; then
         cp -fR $X/schemas docs/$X/
     fi
